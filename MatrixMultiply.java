dc53cac8d44adb7e95bd8575a6cb7f09cda7232d
Roshan Padaki <roshanpadaki@gmail.com>
2017-03-21 22:58:55 -0400

    Fixed errors and tested padding

--- MatrixMultiply.java
+++ MatrixMultiply.java
@@ -3,44 +3,56 @@ import java.io.*;
 
 public class MatrixMultiply {
 	public static void main(String[] args) {
-		int flag=parseInt(args[1]);
+		int flag=parseInt(args[0]);
 
 		// Matrix dimension
-		int n=parseInt(args[2]);
+		int n=parseInt(args[1]);
+
+		// Testing to make sure that
+		// padding was working as desired
+		if (flag == 3) {
+			int crossover = parseInt(args[2]);
+			System.out.println(pad(n,crossover));
+			return;
+		}
+
+		// We set the crossover point here
+		int crossover;
+		
 
 		// Initialize matrices
-		int[][] A = new int[n][n];
-		int[][] B = new int[n][n];
+		int[][] a = new int[n][n];
+		int[][] b = new int[n][n];
 
 		// Get matrices from input file
 		if (flag == 0) {
 			// Input filename
-			String inputfile = args[3];
+			String inputfile = args[2];
 			try {
 				BufferedReader in = new BufferedReader(new FileReader(inputfile));
 				for (int i=0; i<n; i++) {
-					A[i][j] = parseInt(in.readLine());
+					for (int j=0; j<n; j++) {
+						a[i][j] = parseInt(in.readLine());
+					}
 				}
-				i = 0;
-				while (i < n) {
-					B[i][j] = parseInt(in.readLine());
+				for (int i=0; i<n; i++) {
+					for (int j=0; j<n; j++) {
+						b[i][j] = parseInt(in.readLine());
+					}
 				}
 			} catch (IOException e) {
 				System.out.println("Error: Incorrect input format.");
 				System.exit(0);
-			} finally {
-				if (in != null) {
-					in.close();
-				}
 			}
 		}
 		// Randomly generate matrices
 		else if (flag == 1) {
-			int min = parseInt(args[3]);
-			int max = parseInt(args[4]);
-			randMatrix(n,min,max,A);
-			randMatrix(n,min,max,B);
+			int min = parseInt(args[2]);
+			int max = parseInt(args[3]);
+			randMatrix(n,min,max,a);
+			randMatrix(n,min,max,b);
 		}
+
 	}
 
 	// Generate a random matrix and store it in c
@@ -52,6 +64,19 @@ public class MatrixMultiply {
 				c[i][j] = rng.nextInt(range) + min;
 			}
 		}
+	}
+
+	// Figure out how much to pad the matrix to
+	// make Strassen's algorithm run effortlessly
+	public static int pad(int n, int crossover) {
+		int m = 1;
+		while (m * crossover < n) {
+			m = m*2;
+		}
+		while (m * crossover >= n) {
+			crossover = crossover-1;
+		}
+		return (crossover + 1)*m;
 	}
 
 	// Add a len x len submatrix of a to
@@ -99,13 +124,13 @@ public class MatrixMultiply {
 		}
 	}
 
-	public static void strassen(int[][] a, int[][] b, int[][] c, ArrayList<int[][][]> Matrices, int ai, int aj, int bi, int bj, int ci, int cj, int len, int crossover, int depth) {
+	public static void strassen(int[][] a, int[][] b, int[][] c, ArrayList<int[][][]> matrices, int ai, int aj, int bi, int bj, int ci, int cj, int len, int crossover, int depth) {
 		if (len <= crossover) {
 			regMult(a, b, c, ai, aj, bi, bj, ci, cj, len);
 			return;
 		}
 
-		m = Matrices.get(depth);
+		m = matrices.get(depth);
 
 		// p6
 		// B - D goes i m0
@@ -113,19 +138,19 @@ public class MatrixMultiply {
 		// G + H goes in m1
 		add(b, b, m[1], bi, bj+len/2, bi+len/2, bj+len/2, 0, 0, len/2);
 		// multiply goes in m2
-		strassen(m[0], m[1], m[2], Matrices, 0, 0, 0, 0, 0, 0, len/2, depth + 1);
+		strassen(m[0], m[1], m[2], matrices, 0, 0, 0, 0, 0, 0, len/2, depth + 1);
 
 		//p2
 		// A+B goes in m0
 		add(a, a, m[0], ai, aj, ai + len/2, aj, 0, 0, len/2);
 		// multiply goes in m1
-		strassen(m[0], b, m[1], Matrices, 0, 0, bi+len/2, bj+len/2, 0, 0, len/2, depth + 1);
+		strassen(m[0], b, m[1], matrices, 0, 0, bi+len/2, bj+len/2, 0, 0, len/2, depth + 1);
 
 		//p4
 		// G-E goes in m0
 		sub(b, b, m[0], bi + len/2, bj, bi, bj, 0, 0, len/2);
 		// multiply goes in m3
-		strassen(a, m[0], m[3], Matrices, ai+len/2, aj+len/2, 0, 0, 0, 0, len/2, depth+1);
+		strassen(a, m[0], m[3], matrices, ai+len/2, aj+len/2, 0, 0, 0, 0, len/2, depth+1);
 
 		//now we don't need B or G
 		//p6 moves to B's spot
@@ -139,19 +164,19 @@ public class MatrixMultiply {
 		//E+F goes in m1
 		add(b, b, m[1], bi, bj, bi, bj+len/2, len/2);
 		//multiply goes in m2
-		strassen(m[0], m[1], m[2], Matrices, 0, 0, 0, 0, 0, 0, len/2, depth+1);
+		strassen(m[0], m[1], m[2], matrices, 0, 0, 0, 0, 0, 0, len/2, depth+1);
 
 		//p1
 		//F-H goes in m0
 		sub(b, b, m[0], bi, bj+len/2, bi+len/2, bj+len/2, 0, 0, len/2);
 		// multiply goes in m1
-		strassen(a, m[0], m[1], Matrices, ai, aj, 0, 0, 0, 0, len/2, depth+1);
+		strassen(a, m[0], m[1], matrices, ai, aj, 0, 0, 0, 0, len/2, depth+1);
 
 		//p3
 		//C+D goes in m0
 		add(a, a, m[0], ai+len/2, aj, ai+len/2, aj+len/2, len/2);
 		//multiply goes in m4
-		strassen(m[0], b, m[4], Matrices, 0, 0, bi, bj, 0, 0, len/2, depth+1);
+		strassen(m[0], b, m[4], matrices, 0, 0, bi, bj, 0, 0, len/2, depth+1);
 
 		//now we don't need C or F
 		//p1 goes in C's spot
@@ -165,7 +190,7 @@ public class MatrixMultiply {
 		//E+H goes in m1
 		add(b, b, m[1], 0, 0, bi+len/2, bj+len/2, 0, 0, len/2);
 		//multiply goes in m4
-		strassen(m[0], m[1], m[4], Matrices, 0, 0, 0, 0, 0, 0, len/2, depth+1);
+		strassen(m[0], m[1], m[4], matrices, 0, 0, 0, 0, 0, 0, len/2, depth+1);
 
 		//p1 is in C's spot
 		//p2 is in G's spot
@@ -200,7 +225,6 @@ public class MatrixMultiply {
 		//subtract p7
 		sub(m[4], m[2], c, 0, 0, 0, 0, ci+len/2, cj+len/2, len/2);
 	}
-}
 
 	public static int parseInt(String s) {
 		try {
